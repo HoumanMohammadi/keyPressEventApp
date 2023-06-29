1531b935e2174df67f5aeb34298cf2b712b926b4
Houman Mohammadi <houmane.el@gmail.com>
2023-06-29 17:38:51 +0200

    first commit

---
+++ src/main/java/com/example/eventhandlingpressbutton/KeyPressEventController.java
@@ -0,0 +1,2 @@
+package com.example.eventhandlingpressbutton;public class KeyPressEventController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.eventhandlingpressbutton {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+                            
+    opens com.example.eventhandlingpressbutton to javafx.fxml;
+    exports com.example.eventhandlingpressbutton;
+}
